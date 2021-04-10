4e898efc9918439aaa60be0b69dcc80d7a074eba
Enmanuel Ramirez <enmanuelr7@gmail.com>
2021-04-10 16:53:57 -0300

    add firestore hook and display images feature

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { firestore } from '../firebase';
+
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firestore
+      .collection(collection)
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        setDocs(documents);
+      });
+    return () => unsubscribe();
+  }, [collection]);
+  return { docs };
+};
+
+export default useFirestore;
